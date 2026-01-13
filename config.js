49bb9f0afb3cfadb7e1bddaa6a763b0760d16224
DominicDev <s0l0estudi4r@gmail.com>
2026-01-13 13:48:08 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'MAROON 5';
+export let nameSongs = [
+	"SUGAR", /*1*/
+	"ANIMALS ", /*2*/
+	"PAYPHONE ", /*3*/
+	"COLD", /*4*/
+	"MOVES LIKE JAGGER", /*5*/
+	"MAPS", /*6*/
+	"THIS LOVE", /*7*/
+	"SHE WILL BE LOVED", /*8*/
+	"DAYLIGHT", /*9*/
+	"BEAUTIFUL GOODBYE", /*10*/
+	"SUNDAY MORNING", /*11*/
+	"GIRLS LIKE YOU", /*12*/
+	"MEMORIES", /*13*/
+	"HARDER TO BREATHE", /*14*/
+	"MAKES ME WONDER", /*15*/
+	"WON'T GO HOME WITHOUT YOU", /*16*/
+	"WAKE UP CALL", /*17*/
+	"MISERY", /*18*/
+	"BEAUTIFUL MISTAKES", /*19*/
+	"ONE MORE NIGHT", /*20*/
+]
